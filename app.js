a8b0db47e34f6b5ec1d58254da19ee36db431628
LeonFBeymar <beymarleonf18@gmail.com>
2022-08-20 22:04:42 -0300

    Mejoras de los endpoints

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-require('dotenv').config()
-console.log(process.env)
-const port = process.env.PORT
+// require('dotenv').config()
+// console.log(process.env)
+const port = 3000
 
 
 //midlewire
@@ -32,13 +32,15 @@ var coleccion = [
                 }
             ]
 
+//Endpoints
 
+
+//GET
 app.get('/lista', (pedido,respuesta) => {
     respuesta.send(coleccion)
 })
 
-
-
+//POST
 app.post('/lista', (pedido,respuesta) => {
     let canciones = pedido.body;
     let cancion = canciones.filter(x => x.nombre)
@@ -48,52 +50,38 @@ app.post('/lista', (pedido,respuesta) => {
     else
     {
         coleccion = coleccion.concat(pedido.body)
-        respuesta.status(201, "created")
-        respuesta.send(pedido.body)
+        respuesta.status(201, "created").send(pedido.body)
     }
 })
-
-app.get('/lista/:name', (pedido,respuesta) => 
-{
+//GET   
+app.get('/lista/:name', (pedido,respuesta) => {
     let name = pedido.params.name
-    let ver = coleccion.some(x => x.nombre == name)
-    if (ver == false) {
-        respuesta.status(404,"No Found").send()    
+    let cancion = coleccion.filter(x => x.nombre == name).at(0)
+    if (cancion == null) {
+        respuesta.status(404,"No Found").send()
     }
-    else
-    {
-        let cancion = coleccion.find(x => x.nombre == name)
-        respuesta.send(cancion.descripcion)
-    }
+    else respuesta.send(cancion.descripcion)
 })
 
-
+//PUT           
 app.put('/lista/:name', (pedido,respuesta) => {
-    let name = pedido.params.name
-    let ifExist = coleccion.some(x => x.nombre == name)
-    if (ifExist == false) {
-        respuesta.status(404, "No Found").send()
-    }
-    else
-    {
-        var cancion = coleccion.find(x => x.nombre == name)
-        var remplazo = pedido.body[0]
-        if (remplazo.nombre != cancion.nombre) {
-            respuesta.status(409, "Conflict").send();
+    let name = pedido.params.name;
+    let cancion = coleccion.filter(x => x.nombre == name).at(0);
+    if (cancion != null) {
+        if (pedido.body[0].nombre == name) {
+            let a = coleccion.indexOf(cancion)
+            coleccion[a].descripcion = pedido.body[0].descripcion
+            coleccion[a].numero = pedido.body[0].numero
+            respuesta.status(204, "No Content").send();
         }
-        else{
-            var pocision = 0;
-            coleccion.forEach((item, index) => {
-                if (item.nombre == remplazo.nombre) {
-                    pocision = index;
-                }
-            });
-            cancion = remplazo;
-            coleccion[pocision] = cancion;
-            respuesta.status(204, "No Content").send()  
-        }
+        else respuesta.status(409, "Conflict").send();
     }
+    else respuesta.status(404, "No Found").send();
+    
 })
+
+
+
 
 app.delete('/lista/:name', (pedido, respuesta) => {
 
@@ -111,104 +99,79 @@ app.delete('/lista/:name', (pedido, respuesta) => {
 //Parte 2
 app.get('/lista/:name/song', (pedido,respuesta) => {
     let name = pedido.params.name
-    if ((coleccion.some(x => x.nombre == name))== true) {
-        let cancion = coleccion.filter(x => x.nombre == name).at(0)
+    let cancion = coleccion.filter(x => x.nombre == name).at(0)
+    if (cancion != true) {
         respuesta.send(cancion.song)
-        console.log(cancion.song)
     }
-    else {
-        respuesta.status(404).send("No se encuentra la lista")
-    }
+    else respuesta.status(404).send("No se encuentra la lista")
 })
 
 app.get('/lista/:name/song/:titulo', (pedido, respuesta) => {
     let name = pedido.params.name
     let title = pedido.params.titulo
-    if ((coleccion.some(x => x.nombre == name))== true) {
-        let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
-        if ((nomAlbum.song.some(x => x.titulo == title)) == true) {
-            let cancion = nomAlbum.song.filter(x => x.titulo == title).at(0)
+    let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
+    if (nomAlbum != null) {
+        let cancion = nomAlbum.song.filter(x => x.titulo == title).at(0)
+        if (cancion != null)
             respuesta.send(cancion)
-        }
-        else respuesta.send(404, "No Found")
+        else respuesta.status(404,"No Found").send("No se encuentra la cancion")
     }
-    else {
-        respuesta.status(404, "No Found").send("No se encuentra la lista")
-    }
+    else respuesta.status(404, "No Found").send("No se encuentra la lista")
 })
 
 
 app.post('/lista/:name/song', (pedido, respuesta) => {
     let name = pedido.params.name
-    if ((coleccion.some(x => x.nombre == name))== true) {
-        let cancion = coleccion.filter(x => x.nombre == name).at(0)
-        if (pedido.body.titulo != null) {
-            coleccion = coleccion.filter(x => x.nombre != name)
-            cancion.song.push(pedido.body)
-            coleccion.push(cancion)
-            respuesta.send(cancion)
+    let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
+    if (nomAlbum != null) {
+        if (pedido.body.titulo != null && pedido.body.titulo != "" ) {
+            nomAlbum.song.push(pedido.body)
+            respuesta.send(nomAlbum)
         }
         else respuesta.status(404).send("Bad request")
     }
-    else {
-        respuesta.status(404).send("No se encuentra la lista")
-    }
+    else respuesta.status(404).send("No se encuentra la lista")
 })
+
+
 
 app.put('/lista/:name/song/:titulo', (pedido, respuesta) => {
     let name = pedido.params.name;
     let title = pedido.params.titulo;
-    if ((coleccion.some(x => x.nombre == name)) == true) {
-
-        let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
-
-        if ((nomAlbum.song.some(x => x.titulo == title)) == true) {
-
+    let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
+    if (nomAlbum!= null) {
+        let cancion = nomAlbum.song.filter(x => x.titulo == title).at(0)
+        if (cancion != null) {
             if (pedido.body.titulo == title) {
-
                 let iColeccion = coleccion.indexOf(nomAlbum)
-                var iCancion = 0;
-                coleccion[iColeccion].song.forEach((element, i) => {
-                if (element.titulo == title) {
-                    iCancion = i;
-                }
-                })
+                let iCancion = nomAlbum.song.indexOf(cancion)
                 coleccion[iColeccion].song[iCancion].artista = pedido.body.artista;
                 coleccion[iColeccion].song[iCancion].añoEdicion = pedido.body.añoEdicion;
                 coleccion[iColeccion].song[iCancion].nomAlbum = pedido.body.nomAlbum;
+                respuesta.send("Actualizado")
             }
             else respuesta.status(404, "No Found").send()
         }
         else respuesta.status(404, "No Found").send()
     }
     else respuesta.status(404, "No Found").send()
-    respuesta.send()
 })
 
 app.delete('/lista/:name/song/:titulo', (pedido, respuesta) => {
     let name = pedido.params.name
     let title = pedido.params.titulo
-    if ((coleccion.some(x => x.nombre == name)) == true) {
-        let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
-
-        if ((nomAlbum.song.some(x => x.titulo == title)) == true) {
-
+    let nomAlbum = coleccion.filter(x => x.nombre == name).at(0)
+    if (nomAlbum!= null) {
+        let cancion = nomAlbum.song.filter(x => x.titulo == title).at(0)
+        if (cancion != null) {
             let iColeccion = coleccion.indexOf(nomAlbum)
-            var iCancion = 0;
-
-            coleccion[iColeccion].song.forEach((element, i) => {
-            if (element.titulo == title) {
-                iCancion = i;
-            }
-            })
-
+            let iCancion = nomAlbum.song.indexOf(cancion)
             coleccion[iColeccion].song.splice(iCancion,1)
-            respuesta.send()
+            respuesta.send("Borrado")
         }
         else respuesta.status(404, "No Found").send()
     }
     else respuesta.status(404, "No Found").send()
-    respuesta.send()
 })
 
 app.listen(port)
