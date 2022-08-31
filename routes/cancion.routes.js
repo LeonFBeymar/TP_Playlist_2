e0fa07d458c19e87884dc2e7156c150b6f88fe61
LeonFBeymar <beymarleonf18@gmail.com>
2022-08-31 10:05:26 -0300

    Endpoints Completados

--- routes/cancion.routes.js
+++ routes/cancion.routes.js
@@ -27,11 +27,6 @@ import Cancion from '../models/cancion.model'
 //Endpoints
 
 //GET
-// router.get('/lista', (req, res) => {
-//     res.send(coleccion)
-//     res.send(accessLogStream)
-// })
-
 router.get('/lista', async (rec, res)=>{
     try{
         const cancion = await Cancion.find()
@@ -42,18 +37,6 @@ router.get('/lista', async (rec, res)=>{
 })
 
 //POST
-// router.post('/lista', (req, res) => {
-//     let canciones = req.body;
-//     let cancion = canciones.find(x => x.nombre)
-//     if (cancion == "") {
-//         res.status(400).send("Bad Request")
-//     }
-//     else {
-//         coleccion = coleccion.concat(req.body)
-//         res.status(201, "created").send(req.body)
-//     }
-// })
-
 router.post('/lista', async(req, res)=>{
     try{
         const cancion = req.body
@@ -65,15 +48,6 @@ router.post('/lista', async(req, res)=>{
 })
 
 //GET   
-// router.get('/lista/:name', (req, res) => {
-//     let name = req.params.name
-//     let cancion = coleccion.find(x => x.nombre == name)
-//     if (cancion == null) {
-//         res.status(404, "No Found").send()
-//     }
-//     else res.send(cancion.descripcion)
-// })
-
 router.get('/lista/:name', async(req, res) => {
     try {
         const name = req.params.name
@@ -83,22 +57,8 @@ router.get('/lista/:name', async(req, res) => {
         res.status(500).send(err)
     }
 })
-//PUT
-// router.put('/lista/:name', (req, res) => {
-//     let name = req.params.name;
-//     let cancion = coleccion.find(x => x.nombre == name)
-//     if (cancion != null) {
-//         if (req.body[0].nombre == name) {
-//             let a = coleccion.indexOf(cancion)
-//             coleccion[a].descripcion = req.body[0].descripcion
-//             coleccion[a].numero = req.body[0].numero
-//             res.status(204, "No Content").send();
-//         }
-//         else res.status(409, "Conflict").send();
-//     }
-//     else res.status(404, "No Found").send();
-// })
 
+//PUT
 router.put('/lista/:name', async(req, res) => {
     try {
         let name = req.params.name
@@ -111,17 +71,7 @@ router.put('/lista/:name', async(req, res) => {
     }
 })
 
-
-
 //DELETE
-// router.delete('/lista/:name', (req, res) => {
-//     if ((coleccion.some(x => x.nombre == req.params.name)) == true) {
-//         coleccion = coleccion.find(x => x.nombre != req.params.name)
-//         res.status(204, "No content").send();
-//     }
-//     else res.status(404, "No found").send()
-// })
-
 router.delete('/lista/:name', async(req, res) => {
     try {
         let name = req.params.name
@@ -140,14 +90,6 @@ router.delete('/lista/:name', async(req, res) => {
 
 
 //PARTE 2
-// router.get('/lista/:name/song', (req, res) => {
-//     let name = req.params.name
-//     let cancion = coleccion.find(x => x.nombre == name)
-//     if (cancion != true) {
-//         res.send(cancion.song)
-//     }
-//     else res.status(404).send("No se encuentra la lista")
-// })
 
 router.get('/lista/:name/song', async(req, res) => {
     try {
@@ -159,20 +101,6 @@ router.get('/lista/:name/song', async(req, res) => {
     }
 })
 
-
-
-// router.get('/lista/:name/song/:titulo', (req, res) => {
-//     let name = req.params.name
-//     let title = req.params.titulo
-//     let nomAlbum = coleccion.find(x => x.nombre == name)
-//     if (nomAlbum != null) {
-//         let cancion = nomAlbum.song.find(x => x.titulo == title)
-//         if (cancion != null)
-//             res.send(cancion)
-//         else res.status(404, "No Found").send("No se encuentra la cancion")
-//     }
-//     else res.status(404, "No Found").send("No se encuentra la lista")
-// })
 
 router.get('/lista/:name/song/:titulo', async(req, res) => {
     try {
@@ -187,28 +115,13 @@ router.get('/lista/:name/song/:titulo', async(req, res) => {
 })
 
 
-
-
-// router.post('/lista/:name/song', (req, res) => {
-//     let name = req.params.name
-//     let nomAlbum = coleccion.find(x => x.nombre == name)
-//     if (nomAlbum != null) {
-//         if (req.body.titulo != null && req.body.titulo != "") {
-//             nomAlbum.song.push(req.body)
-//             res.send(nomAlbum)
-//         }
-//         else res.status(404).send("Bad request")
-//     }
-//     else res.status(404).send("No se encuentra la lista")
-// })
-
 router.post('/lista/:name/song', async(req, res) => {
     try {
         let name = req.params.name  
         let songCancion = req.body
         const cancion = await Cancion.findOne({nombre:name})
-        await cancion.song.push(songCancion)
-        await Cancion.replaceOne({nombre:name}, cancion)
+        await cancion.song.push(songCancion)|
+        await Cancion.findOneAndUpdate({nombre:name}, cancion)
         res.send("Posteado")
     } catch (err) {
         res.status(500).send(err)
@@ -216,64 +129,22 @@ router.post('/lista/:name/song', async(req, res) => {
 })
 
 
-
-// router.put('/lista/:name/song/:titulo', (req, res) => {
-//     let name = req.params.name;
-//     let title = req.params.titulo;
-//     let nomAlbum = coleccion.find(x => x.nombre == name)
-//     if (nomAlbum != null) {
-//         let cancion = nomAlbum.song.find(x => x.titulo == title)
-//         if (cancion != null) {
-//             if (req.body.titulo == title) {
-//                 let iColeccion = coleccion.indexOf(nomAlbum)
-//                 let iCancion = nomAlbum.song.indexOf(cancion)
-//                 coleccion[iColeccion].song[iCancion].artista = req.body.artista;
-//                 coleccion[iColeccion].song[iCancion].añoEdicion = req.body.añoEdicion;
-//                 coleccion[iColeccion].song[iCancion].nomAlbum = req.body.nomAlbum;
-//                 res.send("Actualizado")
-//             }
-//             else res.status(404, "No Found").send()
-//         }
-//         else res.status(404, "No Found").send()
-//     }
-//     else res.status(404, "No Found").send()
-// })
-
-
 router.put('/lista/:name/song/:titulo', async (req, res) => {
     try {
         let name = req.params.name;
-        let titles = req.params.titulo;
+        let title = req.params.titulo;
         let cancionUpdate = req.body;
         let cancion = await Cancion.findOne({nombre:name});
-        let i = await cancion.song.findIndex(x => x.titulo == titles)
-        cancion.song[i] = cancionUpdate
+        let i = await cancion.song.find(x => x.titulo == title)
+        i.artista = cancionUpdate.artista
+        i.añoEdicion = cancionUpdate.añoEdicion                                                                                                                                                                                                                                        
+        i.nomAlbum = cancionUpdate.nomAlbum
         await Cancion.findOneAndUpdate({nombre:name},cancion);
         res.send("Actualizado");
     } catch (err) {
         res.status(500).send(err)
     }
 })
-
-
-
-// router.delete('/lista/:name/song/:titulo', (req, res) => {
-//     let name = req.params.name
-//     let title = req.params.titulo
-//     let nomAlbum = coleccion.find(x => x.nombre == name)
-//     if (nomAlbum != null) {
-//         let cancion = nomAlbum.song.find(x => x.titulo == title)
-
-//         if (cancion != null) {
-//             let iColeccion = coleccion.indexOf(nomAlbum)
-//             let iCancion = nomAlbum.song.indexOf(cancion)
-//             coleccion[iColeccion].song.splice(iCancion, 1)
-//             res.send("Borrado")
-//         }
-//         else res.status(404, "No Found").send()
-//     }
-//     else res.status(404, "No Found").send()
-// })
 
 router.delete('/lista/:name/song/:titulo', async(req, res)=> {
     try {
@@ -282,8 +153,8 @@ router.delete('/lista/:name/song/:titulo', async(req, res)=> {
         const cancion = await Cancion.findOne({nombre:name})
         let songCancion = cancion.song.find(x => x.titulo == title)
         let pocision = cancion.song.indexOf(songCancion)
-        cancion.song.splice(pocision, 1)
-        await Cancion.replaceOne({nombre:name}, cancion)
+        cancion.song.splice(pocision,1)
+        await Cancion.findOneAndUpdate({nombre:name}, cancion)
         res.send("Borrado")
     } catch (error) {
         
